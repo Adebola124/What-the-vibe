fa5da5e8bd6c1a7c336db2ee0f9d181217d62a36
Adebola124 <adeshinaadebola13@gmail.com>
2025-05-12 06:33:44 -0600

    nn

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: ".", // or "public" if that's where it is
   plugins: [react()],
-  server: {
-    port: 5173
-  }
 });
